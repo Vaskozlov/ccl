5ec4e95cbd714b3ba76f6315265cedb6380ed4cc
vaskozlov <vaskozlov3940@gmail.com>
2023-08-07 10:28:03 +0500

    one more attempt

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,5 +10,6 @@ TEST_CASE("Factorials are computed", "[factorial]")
   REQUIRE(factorial(1) == 1);
   REQUIRE(factorial(2) == 2);
   REQUIRE(factorial(3) == 6);
+  REQUIRE(factorial(5) == 120);
   REQUIRE(factorial(10) == 3628800);
 }
